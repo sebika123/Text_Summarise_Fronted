3f41b7a9db2a35848aef73d9cc1b008651e2e6ac
Ashutosh Jha <105535224+thealonemusk@users.noreply.github.com>
2024-01-22 22:40:47 +0545

    updated the firebase content

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-/*
-const firebaseConfig = {
-    apiKey: "AIzaSyB65pqITFlAXpYBjSDiGKF3Fmsql6tnRDY",
-    authDomain: "text-summariser-4cfbb.firebaseapp.com",
-    projectId: "text-summariser-4cfbb",
-    storageBucket: "text-summariser-4cfbb.appspot.com",
-    messagingSenderId: "57961024012",
-    appId: "1:57961024012:web:2162a4745560c755273c29",
-    measurementId: "G-63VFNTR6VC"
-};*/
-/*
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics"; *
-*/
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyA6lV-JC_h3oVKMbHo9XR7vckaRWjuux1s",
     authDomain: "text-summarizer-f78de.firebaseapp.com",
