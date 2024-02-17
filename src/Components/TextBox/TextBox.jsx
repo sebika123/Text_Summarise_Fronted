14e7d224cbda58a697f601c5fb5e3d053e68c381
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2024-02-17 19:53:20 +0530

    trying to make this work

--- src/Components/TextBox/TextBox.jsx
+++ src/Components/TextBox/TextBox.jsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import "./Styles.css";
 import axios from "axios";
 import Footer from "../Footer/Footer";
 import { saveAs } from "file-saver";
 import jsPDF from "jspdf";
-=======
-import React, { useState } from 'react';
-import './Styles.css';
-import axios from 'axios';
-import Footer from '../Footer/Footer';
-import { saveAs } from 'file-saver';
-import jsPDF from 'jspdf';
-
-
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
 
 import Tracker from "./Tracker";
 import { useHistory } from "../HistoryContext";
@@ -30,14 +19,6 @@ const TextBox = () => {
 
   const { addToHistory } = useHistory();
 
-<<<<<<< HEAD
-=======
- 
-
-
-
-  
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
   const promptForFileName = () => {
     const prompt = window.prompt("Enter a file name:");
     if (prompt) {
@@ -51,17 +32,11 @@ const TextBox = () => {
       const textFile = new Blob([summaryData.summary], { type: "text/plain" });
       const fileName = `${downloadFileName}.txt`;
       saveAs(textFile, fileName);
-<<<<<<< HEAD
       addToHistory(summaryData.summary); // Add summary to history
-=======
-      
-     
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
     }
   };
 
   const downloadPDFSummary = () => {
-<<<<<<< HEAD
     promptForFileName();
     if (downloadFileName) {
       const doc = new jsPDF();
@@ -78,27 +53,9 @@ const TextBox = () => {
 
       doc.save(`${downloadFileName}.pdf`);
       addToHistory(summaryData.summary); // Add summary to history
-=======
-    promptForFileName(); 
-  
-    if (downloadFileName) { 
-      const doc = new jsPDF();
-      doc.setFontSize(12); 
-  
-      const textLines = doc.splitTextToSize(summaryData.summary, doc.internal.pageSize.width - 20);
-  
-      textLines.forEach((line, index) => {
-        doc.text(10, 10 + index * 12, line);
-      });
-  
-      doc.save(`${downloadFileName}.pdf`); 
-      
-      
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
     }
   };
 
-<<<<<<< HEAD
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -106,36 +63,6 @@ const TextBox = () => {
       const response = await axios.post("http://127.0.0.1:5000/analyze", {
         rawtext: formData,
         percentage: percentage,
-=======
-  
-
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const data = {
-      rawtext: formData,
-      percentage: percentage,
-     
-    };
-   
-    
-
-    axios
-      .post('http://127.0.0.1:5000/analyze', data)
-      .then((response) => {
-        console.log('Data sent successfully:', response.data);
-        setSummaryData(response.data);
-        setOutputWordCount(response.data.summary.split(' ').length);
-        setShowOutput(true); 
-        
-        
-        document.getElementById('out-box').classList.add('slideInFromBottom');//animation new
-      })
-      .catch((error) => {
-        console.error('Error sending data:', error);
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
       });
 
       setSummaryData(response.data.summary);
@@ -174,7 +101,6 @@ const TextBox = () => {
     <div id="parent">
       <div id="txt-box">
         <form id="inp-box" onSubmit={handleSubmit}>
-<<<<<<< HEAD
           <div>
             <label htmlFor="">Enter Your Text Here</label>
           </div>
@@ -188,24 +114,6 @@ const TextBox = () => {
             Word Count: {inputWordCount}
           </div>
           <div style={{ marginBottom: "10px" }}>
-=======
-          {/* <div >
-            <label htmlFor="">Enter Your Text Or Link Here</label>
-          </div> */}
-
-<div class="input-container">
-<label for="input-text-or-link" >Enter Your Text Or Link Here</label>
- 
-</div>
-          <textarea cols="30" rows="10" id="txt-area" onChange={handleChange}></textarea>
-          {/* <div style={{  marginBottom: '10px' }}>Word Count: {inputWordCount}</div> */}
-
-
-          <div id="wordcount-div">
-  <button id="wordcount-button">Word Count: <span id="wordcount-value">{inputWordCount}</span></button>
-</div>
-          <div style={{  marginBottom: '10px'  }}>
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
             <label htmlFor="">Enter Final Percentage you want</label>
           </div>
           <input
@@ -223,12 +131,7 @@ const TextBox = () => {
         </form>
         {showOutput && (
           <div id="out-box">
-<<<<<<< HEAD
             <div>
-=======
-            {/* <div >
-
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
               <label htmlFor="output-area">Output</label>
             </div> */}
 
@@ -239,16 +142,7 @@ const TextBox = () => {
             <div id="output-area">
               <div id="content">{summaryData.summary}</div>
             </div>
-<<<<<<< HEAD
             <div>Word Count: {outputWordCount}</div>
-=======
-
-            <div id="wordcount-div">
-  <button id="wordcount-button">Word Count: <span id="wordcount-value">{outputWordCount}</span></button>
-</div>
-            {/* <div >Word Count: {outputWordCount}</div> */}
-
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
             <div id="download-buttons">
               <button
                 onClick={downloadTextSummary}
@@ -270,17 +164,11 @@ const TextBox = () => {
           </div>
         )}
       </div>
-<<<<<<< HEAD
       <Tracker
         onNumSummariesChange={handleNumSummariesChange}
         onNumTxtDownloadsChange={handleNumTxtDownloadsChange}
         onNumPdfDownloadsChange={handleNumPdfDownloadsChange}
       />
-=======
-      {/* <Tracker /> */}
-
-     
->>>>>>> 6431780bc39a91ac718e1916d32d1cd0cba3d801
       <Footer />
     </div>
   );
